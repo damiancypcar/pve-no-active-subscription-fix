0ba67e4cbd4d71d2aceddb409824a91d1f05a2da
damiancypcar <damiancypcar@gmail.com>
2024-02-09 03:25:45 +0100

    Change var names

--- pve-no-active-subscription-fix.sh
+++ pve-no-active-subscription-fix.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # ----------------------------------------------------------
 # Author:          damiancypcar
-# Modified:        2023-12-12
-# Version:         1.0
 # PVE Version:     7+
+# Modified:        2024-02-09
+# Version:         1.1
 # Desc:            Fix Proxmox "no active subscription"
 # ----------------------------------------------------------
 
 set -euo pipefail
 
-ORIGPVE="/etc/apt/sources.list.d/pve-enterprise.list"
-NEWPVE="/etc/apt/sources.list.d/pve-no-subscription.list"
-ORIGCEPH="/etc/apt/sources.list.d/ceph.list"
-NEWCEPH="/etc/apt/sources.list.d/ceph-no-subscription.list"
-PVEVER=$(pveversion -v | grep 'proxmox-ve:' | cut -d' ' -f2 | cut -d'.' -f1)
+ORIG_PVE="/etc/apt/sources.list.d/pve-enterprise.list"
+NEW_PVE="/etc/apt/sources.list.d/pve-no-subscription.list"
+ORIG_CEPH="/etc/apt/sources.list.d/ceph.list"
+NEW_CEPH="/etc/apt/sources.list.d/ceph-no-subscription.list"
+PVE_VER=$(pveversion -v | grep 'proxmox-ve:' | cut -d' ' -f2 | cut -d'.' -f1)
 
 # shellcheck disable=SC2046
 if [ $(id -u) -ne 0 ]; then
@@ -22,20 +22,20 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # version check
-if [ "${PVEVER}" -ge 7 ]; then
-    echo "Proxmox VE version: ${PVEVER}"
+if [ "${PVE_VER}" -ge 7 ]; then
+    echo "Proxmox VE version: ${PVE_VER}"
 else
     echo "Require Proxmox VE version 7 or higher"
     exit 1
 fi
 
 echo -e '\n>> Fixing PVE repo...'
-sed -i "s/^deb/#deb/g" ${ORIGPVE}
-echo 'deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription' > ${NEWPVE}
+sed -i "s/^deb/#deb/g" ${ORIG_PVE}
+echo 'deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription' > ${NEW_PVE}
 
 echo '>> Fixing Ceph repo...'
-sed -i "s/^deb/#deb/g" ${ORIGCEPH}
-echo 'deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription' > ${NEWCEPH}
+sed -i "s/^deb/#deb/g" ${ORIG_CEPH}
+echo 'deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription' > ${NEW_CEPH}
 
 # disable "no active subscription" warning in Proxmox GUI
 echo -e '\n>> Disabling "no active subscription" warning message...'
